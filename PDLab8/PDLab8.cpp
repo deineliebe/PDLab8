4f76a6023f9f97b84f37c2623630d1c8b8535e39
nej-ive <nej-ve@users.noreply.github.com>
2023-04-08 14:47:19 +0300

    final version

--- PDLab8/PDLab8.cpp
+++ PDLab8/PDLab8.cpp
@@ -1,5 +1,11 @@
 ﻿#include <iostream>
 
+float rnd(float x)
+{
+    int temp = x * 1000;
+    return temp / 1000.;
+}
+
 float inDegree(float x, int degree)
 {
     float temp = 1;
@@ -9,25 +15,6 @@ float inDegree(float x, int degree)
     }
     return temp;
 }
-
-//float ln(float x)
-//{
-//    x = 2.7;
-//    float temp = 1;
-//    int i = 1;
-//    float sum = 1;
-//    while (abs(temp) > 0.000001)
-//    {
-//        /*sum += temp / i;
-//        temp *= -1 * x;
-//        i++;*/
-//        sum += temp;
-//        temp *= inDegree((x - 1), 2) / (i * inDegree((x + 1), 2));
-//        i += 2;
-//    }
-//    std::cout << sum << '\n';
-//    return sum * 2 * (x - 1) / (x + 1);
-//}
 
 float function(float x)
 {
@@ -63,47 +50,61 @@ float secondDifferenceDeriative(float x, float h)
     return ((function(x - h) - 2 * function(x) + function(x + h)) / (inDegree(h, 2)));
 }
 
-void task1(float h1, float h2, float start, float end)
+void task1(float h, float start, float end)
 {
-    std::cout << "\nStep: " << h1 << '\n';
-    for (float i = start; i < end; i += h1)
+    std::cout << "\nStep: " << h << '\n';
+    for (float i = start; i < end; i += h)
     {
         std::cout << "h = " << i << ":\n";
         std::cout << "First difference relatives:\n";
-        std::cout << "Mean = " << firstDeriative(i) << ", ";
-        std::cout << "left: " << leftDifferenceDerivative(i, h1) << ", ";
-        std::cout << "central: " << centralDifferenceDerivative(i, h1) << ", ";
-        std::cout << "right: " << rightDifferenceDerivative(i, h1) << "\n";
+        std::cout << "Mean = " << rnd(firstDeriative(i)) << ", ";
+        std::cout << "left: " << rnd(leftDifferenceDerivative(i, h)) << ", ";
+        std::cout << "central: " << rnd(centralDifferenceDerivative(i, h)) << ", ";
+        std::cout << "right: " << rnd(rightDifferenceDerivative(i, h)) << "\n";
         std::cout << "Second relative:\n";
-        std::cout << "Mean = " << secondDeriative(i) << ", ";
-        std::cout << "second difference deriative = " << secondDifferenceDeriative(i, h1) << "\n";
+        std::cout << "Mean = " << rnd(secondDeriative(i)) << ", ";
+        std::cout << "second difference deriative = " << rnd(secondDifferenceDeriative(i, h)) << "\n\n";
     }
+}
 
-    std::cout << "\nStep: " << h2 << '\n';
-    for (float i = start; i < end; i += h2)
+void task2(float h, float start, float end)
+{
+    float result;
+    result = function(start);
+    result += function(end);
+    for (float i = start; i < end; i += h)
     {
-        std::cout << "h = " << i << ":\n";
-        std::cout << "First difference relatives:\n";
-        std::cout << "Mean = " << firstDeriative(i) << ", ";
-        std::cout << "left: " << leftDifferenceDerivative(i, h2) << ", ";
-        std::cout << "central: " << centralDifferenceDerivative(i, h2) << ", ";
-        std::cout << "right: " << rightDifferenceDerivative(i, h2) << "\n";
-        std::cout << "Second relative:\n";
-        std::cout << "Mean = " << secondDeriative(i) << ", ";
-        std::cout << "second difference deriative = " << secondDifferenceDeriative(i, h2) << "\n";
+        result += 4 * function(i - 0.5);
     }
-
-
+    for (float i = start; i < end; i += h)
+    {
+        result += 2 * function(i);
+    }
+    std::cout << "Result: " << rnd((h / 6.) * result);
 }
 
 int main()
 {
-    std::cout << "This is the nineth lab of project design.\n\n";
-    std::cout << "First task is to build right, left and central difference derivatives of function. Find the second difference derivative of function. Compare with correct derivate.\n";
-    std::cout << "Function: ln(x^2 + sqrt(x^4 + 1)).\n";
+    std::cout << "This is the eigth lab of project design.\n\n";
     float h1 = 0.3;
     float h2 = 0.05;
-    task1(h1, h2, 1, 2.5);
-
-    std::cout << "\n\n" << log(2.7) << ' ' << log(1);
+    float start = 1;
+    float end = 2.5;
+    std::cout << "Function: ln(x^2 + sqrt(x^4 + 1)).\n";
+    std::cout << "Interval = [" << start << ", " << end << "]\n";
+    std::cout << "h1 = " << h1 << ", h2 = " << h2 << '\n';
+    std::cout << "First task is to build right, left and central difference derivatives of function. Find the second difference derivative of function. Compare with correct derivate.\n";
+    task1(h1, start, end);
+    task1(h2, start, end);
+    std::cout << "\nSecond task is to integrate function on an interval.\n";
+    if (h1 < h2)
+    {
+        std::cout << "Step: " << h2 << '\n';
+        task2(h2, start, end);
+    }
+    else
+    {
+        std::cout << "Step: " << h1 << '\n';
+        task2(h1, start, end);
+    }
 }
